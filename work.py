c548b21b071912ebe5ff091c409a1309c67e2ade
Martin Fixman <martinfixman@gmail.com>
2024-04-04 17:31:10 +0100

    Logging images with results.

--- work.py
+++ work.py
@@ -1,15 +1,19 @@
 import logging
+import numpy
 import os
 import pickle
+import seaborn
 import torch
 import wandb
 
 from PIL import Image
+from numpy import array
 from torch import nn, tensor
 from torch.optim import Adam
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from torchvision.models.segmentation import fcn_resnet50, FCN_ResNet50_Weights
+from torchvision.transforms.functional import to_pil_image
 from wandb import Artifact
 
 device = 'cuda'
@@ -21,12 +25,10 @@ class CityScapesDataset(Dataset):
     ])
     mask_transform = transforms.Compose([
         transforms.Resize((256, 256), interpolation=transforms.InterpolationMode.NEAREST),
-        transforms.ToTensor(),
-        lambda x: x.squeeze(0),
+        lambda x: torch.from_numpy(array(x)).long(),
     ])
 
     def __init__(self, image_dir, mask_dir, n = None):
-
         self.images = []
         self.masks = []
         for city in os.listdir(image_dir):
@@ -51,7 +53,7 @@ class CityScapesDataset(Dataset):
         image_path = self.images[idx]
         mask_path = self.masks[idx]
         image = Image.open(image_path).convert('RGB')
-        mask = Image.open(mask_path).convert('L')
+        mask = Image.open(mask_path)
 
         if self.transform:
             image = self.transform(image)
@@ -62,24 +64,23 @@ class CityScapesDataset(Dataset):
         return image, mask
 
 class Trainer:
-    def __init__(self, train_dataloader, val_dataloader):
+    def __init__(self, train_dataloader, val_dataloader, config):
         self.train_dataloader = train_dataloader
         self.val_dataloader = val_dataloader
 
-        self.model = self.get_model(30)
+        self.model = self.get_model(34)
         wandb.watch(self.model, log = 'all', log_freq = 10)
 
         self.optimizer = Adam(self.model.parameters(), lr=0.001)
-        self.criterion = torch.nn.CrossEntropyLoss(reduction = 'sum')
+        self.criterion = torch.nn.CrossEntropyLoss(reduction = 'sum', ignore_index = config['ignore_index'])
 
     @staticmethod
     def get_model(num_classes):
-        model = fcn_resnet50(weights = None, progress = True)
+        model = fcn_resnet50(weights = None, progress = True, num_classes = 34)
         model.classifier[4] = nn.Conv2d(512, num_classes, kernel_size = (1, 1))
         return model.to(device)
 
     def log_model(self, **metadata):
-        torch.save(self.model.state_dict(), 'model.pth')
         artifact = Artifact('model_weights', type = 'model', metadata = metadata)
         artifact.add_file('model.pth')
         wandb.log_artifact(artifact)
@@ -101,7 +102,7 @@ class Trainer:
     def run_epoch(self, dataloader, training):
         total_loss = tensor(0.).to(device)
         for e, (images, masks) in enumerate(dataloader, start = 1):
-            images, masks = images.to(device), masks.to(device).long()
+            images, masks = images.to(device), masks.to(device)
             loss = self.run_step(images, masks, training)
             logging.info(f'Running {e}/{len(dataloader)}: partial loss = {loss / len(images):g}')
 
@@ -109,14 +110,41 @@ class Trainer:
 
         return total_loss / len(dataloader.dataset)
 
+    @staticmethod
+    def apply_palette(mask):
+        palette = (255 * array(seaborn.color_palette('husl', 34))).astype(numpy.uint8)
+        color_mask = numpy.zeros((*mask.shape, 3), dtype = numpy.uint8)
+
+        for label in range(34):
+            color_mask[mask == label] = palette[label]
+
+        return color_mask
+
+    def get_sample(self):
+        image, mask_true = [x[0] for x in next(iter(self.val_dataloader))]
+        with torch.no_grad():
+            mask_pred = self.model(image.unsqueeze(0).to(device))['out'].squeeze(0).argmax(dim = 0).cpu()
+
+        return {
+            'Image': wandb.Image(to_pil_image(image)),
+            'True Mask': wandb.Image(to_pil_image(self.apply_palette(mask_true))),
+            'Pred Mask': wandb.Image(to_pil_image(self.apply_palette(mask_pred))),
+        }
+
     def train(self, epochs):
         best_loss = float('inf')
         for epoch in range(1, epochs + 1):
             train_loss = self.run_epoch(self.train_dataloader, training = True)
             val_loss = self.run_epoch(self.val_dataloader, training = False)
 
+            sample = self.get_sample()
+            losses = {
+                'Train loss': train_loss,
+                'Val loss': val_loss,
+            }
+
             logging.info(f'Epoch {epoch}/{epochs}: train loss = {train_loss:g}, val loss = {val_loss:g}')
-            wandb.log({'Epoch': epoch, 'Train loss': train_loss, 'Val loss': val_loss})
+            wandb.log({'Epoch': epoch} | sample | losses)
 
             if val_loss < best_loss:
                 logging.info('Best loss found!')
@@ -125,9 +153,10 @@ class Trainer:
 
 def main():
     config = dict(
-        n = None,
+        n = 10,
         batch_size = 232,
-        epochs = 100,
+        epochs = 3,
+        ignore_index = 0,
     )
 
     wandb.init(
@@ -146,7 +175,7 @@ def main():
     train_dataloader = DataLoader(train_dataset, batch_size = config['batch_size'], shuffle = True)
     val_dataloader = DataLoader(val_dataset, batch_size = config['batch_size'], shuffle = True)
 
-    trainer = Trainer(train_dataloader, val_dataloader)
+    trainer = Trainer(train_dataloader, val_dataloader, config)
     trainer.train(epochs = config['epochs'])
 
 if __name__ == '__main__':
