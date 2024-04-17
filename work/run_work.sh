8ab303f8260b70e0d1f9c800ee773ee438d3dd05
Martin Fixman <martin.fixman@city.ac.uk>
2024-04-17 18:05:39 +0100

    Adding sh files to repo.

--- work/run_work.sh
+++ work/run_work.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name work                      # Job name
-#SBATCH --partition=preemptgpu                         # Select the correct partition.
+#SBATCH --partition=prigpu                         # Select the correct partition.
 #SBATCH --nodes=1                                # Run on 1 nodes (each node has 48 cores)
 #SBATCH --ntasks-per-node=1                        # Run one task
 #SBATCH --cpus-per-task=4                          # Use 4 cores, most of the procesing happens on the GPU
 #SBATCH --mem=72GB                                 # Expected ammount CPU RAM needed (Not GPU Memory)
 #SBATCH --time=72:00:00                            # Expected ammount of time to run Time limit hrs:min:sec
-#SBATCH --gres=gpu:a100:1                               # Use one gpu.
+#SBATCH --gres=gpu:1                               # Use one gpu.
 #SBATCH -e results/%x_%j.e                         # Standard output and error log [%j is replaced with the jobid]
 #SBATCH -o results/%x_%j.o                         # [%x with the job name], make sure 'results' folder exists.
-#SBATCH --error work_preempt.err
-#SBATCH --output work_preempt.out
+#SBATCH --error work.err
+#SBATCH --output work.out
 
 #Enable modules command
 
