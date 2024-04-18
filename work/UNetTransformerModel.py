e8f0ff921629100af684f224a83462e7916e375d
Martin Fixman <martinfixman@gmail.com>
2024-04-18 15:43:39 +0100

    Trying hude model with pretrained weights.

--- work/UNetTransformerModel.py
+++ work/UNetTransformerModel.py
@@ -33,7 +33,7 @@ class Upsampler(nn.Module):
 class Transformer(nn.Module):
     def __init__(self):
         super().__init__()
-        self.transformer = torchvision.models.vit_l_32()
+        self.transformer = torchvision.models.vit_h_14(weights = torchvision.models.ViT_H_14_Weights.DEFAULT)
         self.transformer.heads = nn.Identity()
 
     def forward(self, x):
