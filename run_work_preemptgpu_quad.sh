0468443f9506eb4028be734daf4da978c7d68da6
Martin Fixman <martinfixman@gmail.com>
2024-05-02 15:37:03 +0100

    Adding more things.

--- run_work_preemptgpu_quad.sh
+++ run_work_preemptgpu_quad.sh
@@ -22,5 +22,5 @@ export https_proxy=http://hpc-proxy00.city.ac.uk:3128
 export TORCH_HOME=/mnt/data/public/torch
 export CUDA_LAUNCH_BLOCKING=1
 
-python work.py "$@" --model UNet
-python work.py "$@" --model Swin2Base
+python work.py "$@" --model UNetNoBatchNorm --no-resize --batch-size 4 --epochs 40 --lr 0.001 --gamma 0.31622 --weight-decay 0.05
+python work.py "$@" --model Swin2Base --no-resize --batch-size 4 --epochs 40 --lr 0.001 --gamma 1 --weight-decay 0.0001 --dropout 0.05
